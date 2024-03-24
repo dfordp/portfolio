b3bf955b8e68e1e9aed2e01a324a771c98c2026b
Dilpreet Grover <92905896+dfordp@users.noreply.github.com>
2024-03-24 19:55:26 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -16,7 +16,7 @@ const projects = [
   },
   {
     name : "One Med (Personal Medical Repository)", 
-    description : "An Application which allows one to manage their own & their closed one's medical records with link sharing and onapp storage",
+    description : "An Application which allows one to manage one's medical records with link-sharing and in-app storage",
     github_link : "https://github.com/dfordp/one-med-client",
     deployed_link : "",
   },
