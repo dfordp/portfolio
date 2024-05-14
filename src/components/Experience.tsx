cf56ee3a9189a67e672c2a3e1c7d3afc354bdc4a
Dilpreet <92905896+dfordp@users.noreply.github.com>
2024-05-14 09:52:45 +0530

    made project card mobile optimized

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -16,7 +16,7 @@ const experiences = [
     points : [
       "Orchestrated and coordinated marketing events, contributing significantly to the success of the college's technical fest, Digi Fiesta.",
       "Played a key role in securing sponsorship for the event and successfully invited Sandeep Jain Sir to deliver an insightful tech talk at the university",
-      " Established connections with a diverse range of tech enthusiasts and industry experts, enhancing networking opportunities and contributing to the dynamic tech community."
+      "Established connections with a diverse range of tech enthusiasts and industry experts, enhancing networking opportunities and contributing to the dynamic tech community."
 
     ]
   },
@@ -61,7 +61,7 @@ const Experience = () => {
       <div className="flex flex-col items-center relative">
         <div className="absolute w-0.5 bg-gray-400 top-0 bottom-0 left-1/2"></div>
         {experiences.map((experience, index) => (
-          <div key={index} className={`relative w-full flex ${index % 2 === 0 ? 'justify-start text-left pr-6' : 'justify-end text-left pl-6'} mb-8`}>
+          <div key={index} className={`relative w-full flex ${index % 2 === 0 ? 'justify-start text-left pr-6' : 'justify-end text-left pl-6'} mb-8 items`}>
             <div className="absolute -top-2.5 left-1/2 transform -translate-x-1/2 w-5 h-5 bg-white border-2 border-black dark:border-white rounded-full"></div>
             <div className="w-1/2 md:px-4">
               <h2 className="text-sm md:text-lg font-bold">{experience.company}</h2>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -64,12 +64,12 @@ const Projects = () => {
       </h1>
       <div className="flex flex-row gap-4 py-10 px-5 overflow-x-scroll custom-scrollbar">
         {projects.map((project, index) => (
-          <div key={index} className="w-80 h-56 flex-none dark:bg-[rgba(0,0,0,0.05)] outline outline-1 rounded-md">
+          <div key={index} className="w-48 md:w-80 md:h-56 flex-none dark:bg-[rgba(0,0,0,0.05)] outline outline-1 rounded-md">
             <div className="pt-3 flex flex-row">
               <p className="leading-7 [&:not(:first-child)]:pt-1 pl-4 font-bold ">{project.name}</p>
             </div>
             <p className="leading-7 [&:not(:first-child)]:py-2 pl-4 pr-2 font-medium text-xs">{project.description}</p>
-            <div className="flex flex-row justify-between p-4">
+            <div className="flex flex-col gap-3 md:flex-row md:justify-between p-4">
               <Button onClick={() => handleClick(project.github_link)} className=''>GitHub</Button>
               {project.deployed_link && <Button onClick={() => handleClick(project.deployed_link)}>Deployed</Button>}
             </div>
