08f9c8ad501ff6fac1771f121bdd66c29787636c
Dilpreet <92905896+dfordp@users.noreply.github.com>
2024-06-23 21:33:11 +0530

    seperated data

---
+++ src/data/index.ts
@@ -0,0 +1,53 @@
+export const projects = [
+    {
+      name: "Aarchid", 
+      description: "Provide plant care assistance with in-app chat and health checkups using Google's Gemini Pro models for vision and text analysis.",
+      github_link : "https://github.com/dfordp/aarkid-client",
+      deployed_link : "https://aarkid-client.vercel.app",
+    },
+    {
+      name : "HackMate", 
+      description : "A platform facilitating collaborative learning and innovation among a diverse technical community for hackathon events.",
+      github_link : "https://github.com/dfordp/Hackmate-Intial-working-",
+      deployed_link : "",
+    },
+    {
+      name : "One Med", 
+      description : "A personal medical repo which allows one to manage their own & their family's records with link sharing and in-app storage",
+      github_link : "https://github.com/dfordp/one-med-client",
+      deployed_link : "https://one-med-client.vercel.app",
+    },
+    {
+      name : "Compile Me Playground", 
+      description : "A codepen playground which compiles programs written in multiple popular languages & runs them at near-native speed",
+      github_link : "https://github.com/Ankan002/compile-me-playground",
+      deployed_link : "https://playground.compile-me.com/",
+    },
+    {
+      name : "E-commerce Admin Dashboard CMS", 
+      description : "The service offers a comprehensive dashboard for retailers, supporting both product management and sales analysis.",
+      github_link : "https://github.com/dfordp/e-commerce-admin",
+      deployed_link : "https://e-commerce-admin-dfordp.vercel.app/",
+    },
+    {
+      name : "AI Companion", 
+      description : "A platform which uses Large Language Models(LLMs) to create interactive chat-bots based on real-world personalities.",
+      github_link : "https://github.com/dfordp/langchain-project",
+      deployed_link : "https://langchain-project-two.vercel.app/",
+    },
+    {
+      name : "AI summariser", 
+      description : "GPT-4 Summarizer: Automatically generates concise summaries of articles via API integration, enhancing content digestion and analysis",
+      github_link : "https://github.com/dfordp/ai-summariser",
+      deployed_link : "https://ai-summariser.vercel.app/",
+    },
+    {
+      name : "Messenger App", 
+      description : "Implemented real-time chat features, instantaneous message delivery, synchronized data, private messaging, and scalable group chat capabilities.",
+      github_link : "https://github.com/dfordp/messenger-app",
+      deployed_link : "https://messenger-app-pi.vercel.app/",
+    },
+  ]
+
+
+  
