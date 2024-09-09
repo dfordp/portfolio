dccbc9fe3446a778cad35b2e8e0c8e132d6e6094
Dilpreet <92905896+dfordp@users.noreply.github.com>
2024-09-09 10:53:57 +0530

    added analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import App from './App.tsx'
 import './index.css'
 import { ThemeProvider } from "./components/theme-provider"
 import { CustomToaster } from './components/elements/Toaster.tsx'
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <App />
+      <Analytics />
       <CustomToaster/>
     </ThemeProvider>
   </React.StrictMode>,
