d2d62bd88749d85369a9a56c221ecdcbfbdc7488
Dilpreet <92905896+dfordp@users.noreply.github.com>
2025-02-11 23:22:52 +0530

    minor fixes

--- src/components/Aboutme.tsx
+++ src/components/Aboutme.tsx
@@ -1,14 +1,17 @@
 const Aboutme = () => {
-    return (
-      <div className="md:my-24 md:px-24">
-        <h1 className="scroll-m-20 font-extrabold tracking-tight text-2xl lg:text-4xl py-8 flex flex-row justify-center whitespace-nowrap">
-          About Me
-        </h1>
-        <p className="leading-7 [&:not(:first-child)]:mt-4 text-xs md:text-base dark:text-gray-400">
-          I'm Dilpreet Grover, a dynamic software developer with a fervent passion for technology and innovation. I specialize in in both client and server-side development & deployment technologies and have hands-on experience in Software Engineering, & Freelancing. Beyond academia and professional roles, I thrive in hackathons, sharpening my problem-solving skills and fostering a collaborative spirit. As an open-source advocate and technical writer, I'm excited to explore new technologies and make meaningful contributions in the ever-evolving landscape of software development.         
-        </p>
-      </div>
-    )
-  }
-  
-  export default Aboutme;
+  return (
+    <div className="md:my-24 md:px-24">
+      <h1 className="scroll-m-20 font-extrabold tracking-tight text-2xl lg:text-4xl py-8 flex flex-row justify-center whitespace-nowrap">
+        About Me
+      </h1>
+      <p className="leading-7 [&:not(:first-child)]:mt-4 text-xs md:text-base dark:text-gray-400">
+        I'm <span className="font-semibold">Dilpreet Grover</span>, a dynamic <span className="font-semibold">software developer</span> with a fervent passion for <span className="font-semibold">technology</span> and <span className="font-semibold">innovation</span>. I specialize in both <span className="font-semibold">client</span> and <span className="font-semibold">server-side development</span> & deployment technologies and have hands-on experience in <span className="font-semibold">Software Engineering</span> & <span className="font-semibold">Freelancing</span>.
+      </p>
+      <p className="leading-7 [&:not(:first-child)]:mt-4 text-xs md:text-base dark:text-gray-400">
+        Beyond academia and professional roles, I thrive in <span className="font-semibold">hackathons</span>, sharpening my <span className="font-semibold">problem-solving skills</span> and fostering a <span className="font-semibold">collaborative spirit</span>. As an <span className="font-semibold">open-source advocate</span> and <span className="font-semibold">technical writer</span>, I'm excited to explore new technologies and make meaningful contributions in the ever-evolving landscape of software development.
+      </p>
+    </div>
+  );
+}
+
+export default Aboutme;
