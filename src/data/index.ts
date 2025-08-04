bbc7f414e40ec19259efb79e33d17318e4f57e03
Dilpreet <92905896+dfordp@users.noreply.github.com>
2025-08-04 16:22:55 +0530

    :)

--- src/data/index.ts
+++ src/data/index.ts
@@ -78,7 +78,7 @@ export const projects = [
     {
       name: "Spotify Shadow Listener MCP",
       illustration : SpotifyMCP,
-      description: "A Model Context Protocol (MCP) server that analyzes Spotify listening history, revealing listener identity, taste shifts, and future music trends without requiring Spotify Premium.",
+      description: "A Model Context Protocol (MCP) that analyzes Spotify listening history, revealing listener identity, taste shifts, and future music trends without Spotify Premium.",
       github_link : "https://github.com/dfordp/spotify-shadow-listen-mcp",
       deployed_link : "",
     },
